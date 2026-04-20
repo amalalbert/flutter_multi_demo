25b9fababbf4dceb6c86403aef08b5bc84b32865
IN-LP-239\Amal Albert <amal.albert@innovaturelabs.com>
2026-04-20 16:27:28 +0530

    ui updated

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_rearch/flutter_rearch.dart';
 import 'package:rearch_demo/features/home/ui/auto_banner_slider.dart';
 import 'package:rearch_demo/features/home/ui/hero_section.dart';
-import 'package:rearch_demo/features/home/ui/home_page.dart';
 import 'package:rearch_demo/features/home/ui/title_bar.dart';
-import 'package:rearch_demo/logic/nav_capsules.dart';
 // Import your previous capsules and models here
 
 class MainHomeScreen extends RearchConsumer {
@@ -15,14 +13,19 @@ class MainHomeScreen extends RearchConsumer {
     // Access navigation state
 
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: const [
-            HeroBanner(),
-            SizedBox(height: 16),
-            AutoBannerSlider(),
-          ],
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.black,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              HeroBanner(),
+              SizedBox(height: 16),
+              AutoBannerSlider(),
+            ],
+          ),
         ),
       ),
       appBar: const TitleBar(),
